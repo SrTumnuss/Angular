2c1c5e9e2e2927279ce567da3535a1c6e82867ce
Lucas Barros da Silva <131216452+SrTumnuss@users.noreply.github.com>
2024-04-25 20:59:55 -0300

    Implementação rotas passando id na url

--- app/src/app/app.routes.ts
+++ app/src/app/app.routes.ts
@@ -1,12 +1,15 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { RotaComponent } from './components/rota/rota.component';
+import { ClientDetailComponent } from './components/client-detail/client-detail.component';
 
 export const routes: Routes = [
 
 {path: '', component: HomeComponent},
 {path:'nova-rota', component: RotaComponent},
+{path: 'cliente/:id', component: ClientDetailComponent},
 {path: '**', component: HomeComponent}
+
 
 
 ];
